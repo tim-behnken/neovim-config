4c1c1639d5b8555d369bfa7870f848185d5c8143
Tim Behnken <tbehnken@gmail.com>
2025-05-06 14:41:37 -0700

    add better lsp keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -76,6 +76,14 @@ vim.keymap.set("n", "<leader>w", "<cmd>HopWord<CR>", { desc = "Hop to word" })
 vim.keymap.set("n", "<leader>l", "<cmd>HopLine<CR>", { desc = "Hop to line" })
 vim.keymap.set("n", "<leader>c", "<cmd>HopChar2<CR>", { desc = "Hop to char" })
 
+-- lsp
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
+vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { desc = "Go to declaration" })
+vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { desc = "Go to implementation" })
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { desc = "Go to references" })
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { desc = "Show hover" })
+vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "Rename" })
+
 -- -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
